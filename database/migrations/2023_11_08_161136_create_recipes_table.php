fcbfe5b2dfe118707dd3ae12be973a8e0524ff94
Demuniano <jrojasroj@unal.edu.co>
2023-11-08 13:03:13 -0500

    change migration

--- database/migrations/2023_11_08_161136_create_recipes_table.php
+++ database/migrations/2023_11_08_161136_create_recipes_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('idUser');
             $table->string('name');
             $table->float('cantProtein');
             $table->float('cantCarbo');
